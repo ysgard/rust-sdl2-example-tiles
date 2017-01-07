ad6b5b3daeb92cb5c6e0f4c24b391687e20a1d2c
ysgard <ysgard@gmail.com>
2017-01-07 13:03:26 -0800

    Added windows run-time libraries

---
+++ README.md
@@ -0,0 +1,4 @@
+# rust-sdl2-example-tiles
+
+A simple demonstration of how to blit tiles from a spritesheet to the screen using rust-sdl2.
+
--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,10 @@ use std::path::Path;
 use sdl2::pixels::Color;
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
+use sdl2::surface::Surface;
+use sdl2::rect::Rect;
 
-use sdl2::image::{INIT_PNG, LoadTexture};
+use sdl2::image::{INIT_PNG, LoadSurface, LoadTexture};
 
 // Screen dimensions
 const WIDTH:  u32 = 800;
@@ -20,9 +22,7 @@ const HEIGHT: u32 = 600;
 // 0-31 not used, 128-160 blank, 128-139 custom glyphs
 const SPRITE_H: u32 = 28;
 const SPRITE_W: u32 = 18;
-const SPRITE_SHEET: &'static str = "resources/BrogueFont5.png"
-
-
+const SPRITE_SHEET: &'static str = "resources/BrogueFont5.png";
 
 pub fn main() {
     let sdl_context = sdl2::init().unwrap();
@@ -34,12 +34,22 @@ pub fn main() {
         .build()
         .unwrap();
 
-    let _image = sdl2::image::init(INIT_PNG)
+    let _image = sdl2::image::init(INIT_PNG);
 
     let mut renderer = window.renderer().build().unwrap();
 
+    // Load the spritesheet as a surface so we can set the color key
+    let spritesheet = renderer.load_texture(Path::new(SPRITE_SHEET)).unwrap();
+
+    // Create a 'punch' rect for getting individual sprites from the spritesheet
+    let sprite_clip = Rect::new(0, 0, SPRITE_W, SPRITE_H);
+
     renderer.set_draw_color(Color::RGB(0, 0, 0));
     renderer.clear();
+
+    // Copy the sprite(s) onto the window
+    renderer.copy(&spritesheet, None, None).expect("Render failed");
+
     renderer.present();
     let mut event_pump = sdl_context.event_pump().unwrap();
 
