1b1755dc74813956b6304c759506101b07594574
Jan Van Uytven <jvanuytven@change.org>
2017-04-12 18:51:28 -0700

    Proper transparency blitting

--- src/main.rs
+++ src/main.rs
@@ -31,6 +31,14 @@ const SPRITE_COLS: u32 = 16;
 const SPRITE_ROWS: u32 = 16;
 const SPRITE_SHEET: &'static str = "resources/BrogueFont5.png";
 
+/// Pixel Mod Ops
+
+enum PixelMod {
+    Set,
+    Multiply,
+    Add,
+}
+
 
 // General procedure
 // 1. Create a black surface the size of the glyph
@@ -40,25 +48,61 @@ const SPRITE_SHEET: &'static str = "resources/BrogueFont5.png";
 // 5. Create a surface with the bg color we want
 // 6. Blit the colored glyph onto the background tile with blendmode:none
 
+/// Return the individual sprite, with the color key
+///
+/// # Arguments
+///
+/// * spritesheet - the full spritesheet loaded as a Surface
+/// * sprite_rect - a Rect defining the specific sprite's location on the spritesheet
+/// * key - when specified, the color to be set as alpha, or None for default (black)
+///
+/// # Return Value
+/// A Surface with the specific sprite
+fn raw_sprite<'a>(spritesheet: &Surface,
+                  sprite_rect: Rect,
+                  key: Option<Color>)
+                  -> Surface<'a> {
+    let alpha_color = match key {
+        Some(color) => color,
+        None => Color::RGB(0, 0, 0),
+    };
+    let mut tile: Surface = create_tile(sprite_rect, alpha_color);
+    spritesheet.blit(Some(sprite_rect), &mut tile, None).unwrap();
+    tile.set_color_key(true, alpha_color);
+    tile
+}
 
-fn create_sprite<'a>(spritesheet: &Surface,
-                     sprite_rect: Rect,
-                     fg: Color,
-                     bg: Color)
-                     -> Surface<'a> {
-    let BLACK = Color::RGBA(0, 0, 0, 0);
-    let ALPHA = Color::RGBA(0, 0, 0, 0);
-    let WHITE = Color::RGBA(255, 255, 255, 0);
-    let mut tile: Surface = create_tile(sprite_rect, bg);
-    let mut tmp: Surface = create_tile(sprite_rect, WHITE);
-    let mut glyph: Surface = create_tile(sprite_rect, BLACK);
-    spritesheet.blit(Some(sprite_rect), &mut glyph, None).unwrap();
-    glyph.set_color_mod(fg);
-    tmp.set_blend_mode(BlendMode::Add);
-    glyph.blit(None, &mut tmp, None).unwrap();
-    tmp.set_color_key(true, WHITE);
-    tmp.set_blend_mode(BlendMode::None);
-    tmp.blit(None, &mut tile, None).unwrap();
+/// Return a sprite with a colored foreground and background
+///
+/// # Arguments
+///
+/// * sprite   - The sprite to be colored, with the background transparent
+///              (generally should be output of raw_sprite
+/// * fg_color - if not None, the color to tint the sprite before blitting
+///              onto the background
+/// * bg_color - the background of the sprite - if None, the background won't
+///              be set
+///
+/// # Return Value
+///
+/// A new Surface with the tinted sprite blitted onto the colored background
+fn color_sprite<'a>(sprite: &Surface,
+                    fg_color: Option<Color>,
+                    bg_color: Option<Color>)
+                    -> Surface<'a> {
+    let bg: Color = match bg_color {
+        None => Color::RGB(0, 0, 0),
+        Some(c) => c,
+    };
+    let mut tile: Surface = create_tile(Rect::new(0, 0, sprite.width(), sprite.height()), bg);
+    let mut glyph: Surface = create_tile(Rect::new(0, 0, sprite.width(), sprite.height()),
+                                         Color::RGB(0, 0, 0));
+    glyph.set_blend_mode(BlendMode::Blend);
+    sprite.blit(None, &mut glyph, None).unwrap();
+    if fg_color.is_some() {
+        glyph.set_color_mod(fg_color.unwrap());
+    }
+    glyph.blit(None, &mut tile, None).unwrap();
     tile
 }
 
@@ -66,18 +110,21 @@ fn create_tile<'a>(size_rect: Rect, color: Color)
                            -> Surface<'a> {
     let mut tile: Surface = Surface::new(size_rect.width(),
                                          size_rect.height(),
-                                         PixelFormatEnum::ARGB8888
+                                         PixelFormatEnum::RGB888
     ).unwrap();
     tile.fill_rect(None, color);
     tile
 }
     
-    
 pub fn main() {
 
     let RED = Color::RGB(255, 0, 0);
-    let BLUE = Color::RGB(128 ,128, 128);
-
+    let BLUE = Color::RGB(0 , 0, 255);
+    let WHITE = Color::RGB(255, 255, 255);
+    let BLACK = Color::RGB(0, 0, 0);
+    let GREEN = Color::RGB(0, 255, 0);
+    let PINK = Color::RGB(255, 105, 180);
+    
     let sdl_context = sdl2::init().unwrap();
     let video_subsystem = sdl_context.video().unwrap();
 
@@ -102,7 +149,7 @@ pub fn main() {
     // convert the surface to a texture
     let mut spritesheet: Texture = renderer.create_texture_from_surface(&spritesurf).unwrap();
     // Set the blend mode on the texture for antialiasing
-     spritesheet.set_blend_mode(BlendMode::Add);
+    spritesheet.set_blend_mode(BlendMode::Add);
 
 
     // Create a 'punch' rect for getting individual sprites from the spritesheet
@@ -115,7 +162,7 @@ pub fn main() {
 
     // Double buffer it up
     let mut buffer_tex: Texture = renderer.create_texture(
-        PixelFormatEnum::RGBA8888,
+        PixelFormatEnum::RGB888,
         TextureAccess::Target,
         WIDTH,
         HEIGHT
@@ -143,6 +190,7 @@ pub fn main() {
 
             // Set the SDL render target to be our double buffer
             renderer.render_target().unwrap().set(buffer_tex);
+            renderer.set_draw_color(BLUE);
             renderer.clear();
 
             // For each sprite 'block' in the width and height of the screen...
@@ -176,12 +224,14 @@ pub fn main() {
                         (glyph_y * SPRITE_H) as i32,
                         SPRITE_W,
                         SPRITE_H);
-      
-                    let glyph: Surface = create_sprite(&spritesurf,
-                                                       src_rect,
-                                                       rand_rgb_1,
-                                                       rand_rgb_2);
-                    let glyph_tex: Texture = renderer.create_texture_from_surface(&glyph).unwrap();
+
+        
+                    let raw_glyph: Surface = raw_sprite(&spritesurf,
+                                                  src_rect,
+                                                    Some(BLACK));
+                    let glyph: Surface = color_sprite(&raw_glyph, Some(PINK), Some(GREEN));
+                    //let glyph: Surface = color_sprite(&raw_glyph, Some(rand_rgb_1), Some(rand_rgb_2));
+                    let mut glyph_tex: Texture = renderer.create_texture_from_surface(&glyph).unwrap();
                     renderer.copy(&glyph_tex, None, Some(dest_rect)).unwrap();
                     
 
@@ -190,6 +240,7 @@ pub fn main() {
 
             // Flip the double buffer to the screen
             buffer_tex = renderer.render_target().unwrap().reset().unwrap().unwrap();
+            renderer.clear();
             renderer.copy(&buffer_tex, None, None).expect("Render failed");
             renderer.present();
             // done blitting
